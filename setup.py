eed6b79eaebc4c1f2ed60d1a38e5dde5751e591a
hfaran <hamzafaran@hotmail.com>
2013-12-19 19:13:31 -0800

    Add __version__

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+import tornado_json
 
 with open("requirements.txt", "r") as f:
     install_requires = f.read().split()
 
 setup(
     name="Tornado-JSON",
-    version='0.1',
+    version=tornado_json.__version__,
     author='Hamza Faran',
     author_email='hamza@hfaran.com',
     description=('A simple JSON API framework based on Tornado'),
