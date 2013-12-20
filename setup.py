af62a04233d4334a75baa7b47edbb3a069477ba5
hfaran <hamzafaran@hotmail.com>
2013-12-19 16:03:58 -0800

    Pin >= required packages

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().split()
+
 setup(
     name="Tornado-JSON",
     version='0.1',
@@ -7,13 +10,7 @@ setup(
     author_email='hamza@hfaran.com',
     description=('A simple JSON API framework based on Tornado'),
     packages=['tornado_json'],
-    install_requires = [
-        "MySQL-python",
-        "tornado",
-        "dataset",
-        "torndb",
-        "jsonschema",
-    ],
+    install_requires = install_requires,
     data_files=[
         # Populate this with any files config files etc.
     ],
