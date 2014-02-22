36aee385ebb1745f0ca02346b5c55b0c470b1687
hfaran <hamzafaran@hotmail.com>
2014-02-22 13:24:50 -0800

    Add classifiers for 2.7 and 3.3; resolves #33

--- setup.py
+++ setup.py
@@ -61,7 +61,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ]
 )
