36dcf720f57a6cecd1ecd078e003b2e448c6d78b
hfaran <hamzafaran@outlook.com>
2014-11-21 20:16:16 -0800

    Update trove classifiers; setup.py test uses pytest, not tox

--- setup.py
+++ setup.py
@@ -16,20 +16,16 @@ install_requires = read("requirements.txt").split()
 long_description = read('README.md')
 
 
-class Tox(TestCommand):
+class Pytest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        # self.test_args = []
         self.test_args = ['--verbose']
         self.test_suite = True
 
     def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        # import tox
-        # errcode = tox.cmdline(self.test_args)
-
         # Using pytest rather than tox because Travis-CI has issues with tox
+        # Import here, cause outside the eggs aren't loaded
         import pytest
         errcode = pytest.main(self.test_args)
 
@@ -46,19 +42,20 @@ setup(
     long_description=long_description,
     packages=['tornado_json'],
     install_requires = install_requires,
-    tests_require=['tox'],
-    cmdclass = {'test': Tox},
+    tests_require=['pytest'],
+    cmdclass = {'test': Pytest},
     data_files=[
         # Populate this with any files config files etc.
     ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ]
 )
