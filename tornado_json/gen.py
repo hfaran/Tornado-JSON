c736dc8f0217a645abd2aad6392d58e0426453d9
hfaran <hamzafaran@outlook.com>
2015-04-14 00:59:19 -0700

    Support tornado==3.x.x gen.coroutine

--- tornado_json/gen.py
+++ tornado_json/gen.py
@@ -2,12 +2,18 @@ import inspect
 
 from tornado import gen
 
+from tornado_json.constants import TORNADO_MAJOR
+
 
 def coroutine(func, replace_callback=True):
     """Tornado-JSON compatible wrapper for ``tornado.gen.coroutine``
 
     Annotates original argspec.args of ``func`` as attribute ``__argspec_args``
     """
-    wrapper = gen.coroutine(func, replace_callback)
+    # gen.coroutine in tornado 3.x.x has a different signature from 4.x.x
+    if TORNADO_MAJOR == 3:
+        wrapper = gen.coroutine(func)
+    else:
+        wrapper = gen.coroutine(func, replace_callback)
     wrapper.__argspec_args = inspect.getargspec(func).args
     return wrapper
