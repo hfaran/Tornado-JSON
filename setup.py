ceeae59b707facdb4aa70704a3de1778e8174567
Hamza Faran <hamzafaran@outlook.com>
2018-03-18 02:42:57 -0700

    Update python and tornado versions

--- setup.py
+++ setup.py
@@ -54,9 +54,8 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ]
 )
